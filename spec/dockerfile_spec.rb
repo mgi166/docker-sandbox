ac45284fe7f920b54ae29231ae0d25652b53a5f0
mgi166 <skskoari@gmail.com>
2017-03-16 10:00:48 +0900

    Set docker backend setting in spec_helper

--- spec/dockerfile_spec.rb
+++ spec/dockerfile_spec.rb
@@ -1,13 +1,6 @@
-require 'serverspec'
-require 'docker'
-
 describe 'Dockerfile' do
-  before do
-    image = Docker::Image.build_from_dir('.')
-  end
-
   it 'installs ubuntu' do
     os_version = command('lsb_release -a').stdout
-    expect(os_version).to include('ubuntu')
+    expect(os_version).to include('Ubuntu')
   end
 end
